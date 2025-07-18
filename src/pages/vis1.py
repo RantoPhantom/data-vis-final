a66a48238a66c585ea461ffc5537860f4c2b4735
AFanOfHololive <ios2004@icloud.com>
2025-07-18 20:27:48 +0700

    yay

---
+++ src/pages/vis1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title(";adskflaksjdf;lksajdflk")
+st.markdown("this is a tes ")
+
---
+++ src/pages/vis2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title(";adskflaksjdf;lksajdflk")
+st.markdown("aslkdjf;lsakdjf;aslkj")
+
